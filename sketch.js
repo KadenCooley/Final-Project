9d8c8a907a6efb1efbce30d6ca0b61082d955fb3
KadenCooley <119355315+KadenCooley@users.noreply.github.com>
2022-11-29 13:47:30 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,62 @@
+// BOMB GAME
+
+
+
+// starting position variables
+let charX = 50
+let charY = 200
+
+// character axial movement speed
+let charSpeed = 5
+
+//character hitbox radius
+let charBox = 25
+
+
+
 function setup() {
-  // put setup code here
+  createCanvas(1000, 800);
+  frameRate(60);
 }
 
+
+  
 function draw() {
-  // put drawing code here
+  
+  
+  // BACKDROP
+  
+  background(220);
+  
+  // BACKDROP
+  
+  
+    // MOVEMENT CODE
+  
+  // if W go up
+  if (keyIsDown(87) && (charY > charBox))  {
+      charY = (charY - charSpeed);
+  }
+  // if A go left
+  if (keyIsDown(65) && (charX > charBox)) {
+      charX = (charX - charSpeed);
+  }
+  // if S go down
+  if (keyIsDown(83) && (charY < (height - charBox))) {
+      charY = (charY + charSpeed);
+  }
+  // if D go right
+  if (keyIsDown(68) && (charX < (width - charBox))) {
+      charX = (charX + charSpeed);
+  }  
+  
+    // MOVEMENT CODE
+  
+  
+    // DRAW CHARACTER
+  
+  fill(0);
+  circle(charX, charY, 50);
+  
+    // DRAW CHARACTER
 }
