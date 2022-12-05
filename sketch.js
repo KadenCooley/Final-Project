4cb7c52bbc86b48e9b6af21027430ec8208e6971
KadenCooley <119355315+KadenCooley@users.noreply.github.com>
2022-12-04 18:21:39 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,8 @@
 // BOMB GAME
 
 
+// HP bar
+let HP = 100
 
 // starting position variables
 let charX = 50
@@ -29,7 +31,9 @@ function draw() {
   
   // BACKDROP
   
-  background(220);
+  background('#220333');
+  fill(255);
+  rect(10, 80, (width - 25), (height - 100), 25);
   
   // BACKDROP
   
@@ -37,19 +41,19 @@ function draw() {
     // MOVEMENT CODE
   
   // if W go up
-  if (keyIsDown(87) && (charY > charBox))  {
+  if (keyIsDown(87) && (charY > (charBox + 80)))  {
       charY = (charY - charSpeed);
   }
   // if A go left
-  if (keyIsDown(65) && (charX > charBox)) {
+  if (keyIsDown(65) && (charX > (charBox + 10))) {
       charX = (charX - charSpeed);
   }
   // if S go down
-  if (keyIsDown(83) && (charY < (height - charBox))) {
+  if (keyIsDown(83) && (charY < (height - (charBox + 20)))) {
       charY = (charY + charSpeed);
   }
   // if D go right
-  if (keyIsDown(68) && (charX < (width - charBox))) {
+  if (keyIsDown(68) && (charX < (width - (charBox + 15)))) {
       charX = (charX + charSpeed);
   }  
   
@@ -58,8 +62,18 @@ function draw() {
   
     // DRAW CHARACTER
   
-  fill(0);
+  fill(255);
   circle(charX, charY, 50);
   
     // DRAW CHARACTER
+  
+    // DRAW HP
+  
+  fill(0)
+  rect(30, 30, 740, 20, 20);
+  fill('#FF0000')
+  rect(30, 30, HP*7.4, 20, 20);
+  
+    // DRAW HP
+  
 }
